02dde96f7836bafb9c7a9dd63cd12967f8faef24
Roger <rdpender3@gmail.com>
2019-04-06 13:17:06 -0700

    added items for burger builder controls

---
+++ src/components/Burger/BurgerControls/BurgerControl/burgerControl.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./burgerControl.module.css";
+
+const burgerControl = props => (
+  <div className={classes.burgerControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
+  </div>
+);
+
+export default burgerControl;
---
+++ src/components/Burger/BurgerControls/burgerControls.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const burgerControls = props => <div />;
+
+export default burgerControls;
