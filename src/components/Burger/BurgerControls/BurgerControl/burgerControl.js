53d3c87d6129bdf3b3f91f5bf8feab144aa7e584
Roger <rdpender3@gmail.com>
2019-04-06 13:43:39 -0700

    added logic to disable buttons

--- src/components/Burger/BurgerControls/BurgerControl/burgerControl.js
+++ src/components/Burger/BurgerControls/BurgerControl/burgerControl.js
@@ -4,7 +4,11 @@ import classes from "./burgerControl.module.css";
 const burgerControl = props => (
   <div className={classes.burgerControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less} onClick={props.removed}>
+    <button
+      className={classes.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
       Less
     </button>
     <button className={classes.More} onClick={props.added}>
