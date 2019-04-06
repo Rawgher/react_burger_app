fd54ed5617b200b0064f6dbadf13f4dae849ba89
Roger <rdpender3@gmail.com>
2019-04-06 13:34:44 -0700

    added functionality to add ingredients

--- src/components/Burger/BurgerControls/BurgerControl/burgerControl.js
+++ src/components/Burger/BurgerControls/BurgerControl/burgerControl.js
@@ -5,7 +5,9 @@ const burgerControl = props => (
   <div className={classes.burgerControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button className={classes.More} onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
--- src/components/Burger/BurgerControls/burgerControls.js
+++ src/components/Burger/BurgerControls/burgerControls.js
@@ -1,5 +1,24 @@
 import React from "react";
+import BurgerControl from "./BurgerControl/burgerControl";
+import classes from "./burgerControls.module.css";
 
-const burgerControls = props => <div />;
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
+const burgerControls = props => (
+  <div className={classes.BurgerControls}>
+    {controls.map(ctrl => (
+      <BurgerControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+      />
+    ))}
+  </div>
+);
 
 export default burgerControls;
